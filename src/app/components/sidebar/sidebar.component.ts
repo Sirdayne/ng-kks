ebefad817f3e79db3e25ac8958221e476aebf6db
Yernar <yernar.mussin@nitka.com>
2023-10-31 21:03:48 +0600

    routes

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { finalize } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../auth.service';
+import { TokenService } from '../token.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class NavigationComponent {
+  form: FormGroup;
+  submitted = false;
+  loading = false;
+  authError;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private tokenService: TokenService,
+              private snackBar: MatSnackBar) {
+    this.form = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.form.get('email').invalid) {
+      this.authError = 'Введите e-mail';
+      return;
+    }
+    if (this.form.get('password').invalid) {
+      this.authError = 'Введите пароль';
+      return;
+    }
+    this.submitted = true;
+    this.loading = true;
+    this.authError = '';
+    this.authService.login(this.form.getRawValue()).pipe(
+      finalize(() => this.loading = false)
+    ).subscribe((res: any) => {
+
+    }, (err) => {
+      this.authError = err && err.error && err.error.message ? err.error.message : "Неверные email или пароль";
+    });
+  }
+}
